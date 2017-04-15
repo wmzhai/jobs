68d151744716fc83bb2d34f827a3490bc9a4f36a
Weiming Zhai <wmzhai@gmail.com>
2017-04-15 17:47:09 +0800

    welcome screen style

--- screens/welcome_screen.js
+++ screens/welcome_screen.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import Slides from '../components/slides'
+
+const SLIDE_DATA = [
+  { text: 'Welcome to JobApp', color: '#03A9F4'},
+  { text: 'Use this to get a job', color: '#009688'},
+  { text: 'Set your location, then swipe away', color: '#03A9F4'}
+]
 
 class WelcomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-        <Text>WelcomeScreen</Text>
-      </View>
+      <Slides data={SLIDE_DATA} />
     )
   }
 }
