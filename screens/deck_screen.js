edf98daf5ef021b156cbba328071b5eb294ff4e4
Weiming Zhai <wmzhai@gmail.com>
2017-04-16 21:21:04 +0800

    add swipe component

--- screens/deck_screen.js
+++ screens/deck_screen.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { MapView } from 'expo'
+import { Card, Button } from 'react-native-elements'
+import Swipe from '../components/swipe'
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    return (
+      <Card title={job.jobtitle} >
+        <View style={styles.detailWrapper} >  
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b/g, '')}
+        </Text>
+      </Card>
+    )
+  }
+
+  renderNoMoreCards = () => {
+    return (
+      <Card title="No More Jobs">
+      </Card>
+    )
+  }
+
   render() {
     return (
       <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+        <Swipe
+          data={this.props.jobs}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />        
       </View>
     )
   }
 }
 
-export default DeckScreen
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
+}
+
+function mapStateToProps({jobs}) {
+  return { jobs: jobs.results }
+}
+
+export default connect(mapStateToProps)(DeckScreen)
