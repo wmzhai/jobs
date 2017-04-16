1c652a91045fdf08f304f4b8d99fc675f01feda4
Weiming Zhai <wmzhai@gmail.com>
2017-04-16 15:11:49 +0800

    advancing the welcome screen

--- screens/welcome_screen.js
+++ screens/welcome_screen.js
@@ -1,5 +1,7 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, AsyncStorage } from 'react-native'
+import { AppLoading } from 'expo'
 import Slides from '../components/slides'
 
 const SLIDE_DATA = [
@@ -9,11 +11,28 @@ const SLIDE_DATA = [
 ]
 
 class WelcomeScreen extends Component {
+  state = { token: null }
+
+  async componentWillMount() {
+    let token = AsyncStorage.getItem('fb_token')
+
+    if(token) {
+      this.props.navigation.navigate('map')
+      this.setState({token})
+    } else {
+      this.setState({ token: false })  
+    }    
+  }
+
   onSlidesComplete = () => { //由于使用了Arrow Function，所以使用时不需要.bind(this)
     this.props.navigation.navigate('auth')
   }
 
   render() {
+    if(_.isNull(this.state.token)) {
+      return <AppLoading />
+    }
+
     return (
       <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete}/>
     )
